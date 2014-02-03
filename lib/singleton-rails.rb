f9d0555e921b41fe21c1cc65160fc844862e345a
Christoffer Winterkvist <chris@zenangst.com>
2014-02-03 14:21:58 +0100

    Added require 'active_record' to singleton-rails.rb

--- lib/singleton-rails.rb
+++ lib/singleton-rails.rb
@@ -1,2 +1,7 @@
-module SingletonRails
+require 'active_record' rescue nil
+
+if defined? ActiveRecord
+  require "active_record/singleton"
+  require "rails_admin/config/actions/singleton_aware_delete"
+  require "rails_admin/config/actions/singleton_aware_index"
 end
