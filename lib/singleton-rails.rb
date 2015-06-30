b9c9dd2d9394bfaff348a525bd14bad973ff19b5
Tim Kurvers <tim@moonsphere.net>
2015-07-01 00:11:10 +0200

    Normalize quotes

--- lib/singleton-rails.rb
+++ lib/singleton-rails.rb
@@ -1,9 +1,9 @@
 require 'active_record' rescue nil
 
 if defined? ActiveRecord
-  require "active_record/singleton"
+  require 'active_record/singleton'
   if defined? RailsAdmin
-    require "rails_admin/config/actions/singleton_aware_delete"
-    require "rails_admin/config/actions/singleton_aware_index"
+    require 'rails_admin/config/actions/singleton_aware_delete'
+    require 'rails_admin/config/actions/singleton_aware_index'
   end
 end
